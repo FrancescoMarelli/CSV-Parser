b4baa35c09b7e0856505e98363fb02410928329d
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-10-05 15:29:21 +0100

    Context and name formatting

--- DAP-Practica02/src/Practica2/JCharts/BarChart.java
+++ DAP-Practica02/src/Practica2/JCharts/BarChart.java
@@ -12,23 +12,28 @@ import java.awt.*;
 public class BarChart extends Chart {
     public BarChart(String applicationTitle, String chartTitle, CSVParser file, int xAxis, int yAxis) {
         super(applicationTitle, xAxis, yAxis);
-        if(xAxis > file.getHeaders().size() || yAxis > file.getHeaders().size()) throw new IndexOutOfBoundsException();
+        if(xAxis > file.getCSVFile().getHeaders().size() || yAxis > file.getCSVFile().getHeaders().size()) throw new IndexOutOfBoundsException();
+
         createDataBase = new ChartDataBase();
         JFreeChart barChart = ChartFactory.createBarChart(
                 chartTitle,
                 // X axis
-                file.getHeaders().get(xAxis),
-                // Y axis
-                file.getHeaders().get(yAxis),
+                file.getCSVFile().getHeaders().get(xAxis),
+                // Y axis=
+                file.getCSVFile().getHeaders().get(yAxis),
                 createDataBase.createDataBase(file, xAxis, yAxis),
                 PlotOrientation.VERTICAL,
                 true, true, false);
+
+        // Add the chart to a panel to display it
         ChartPanel chartPanel = new ChartPanel(barChart);
         chartPanel.setPreferredSize(new Dimension( 600 ,400 ) );
         setContentPane(chartPanel);
     }
 
     public void display() {
+        // Display the chart in a window frame (JFrame) and center it on the screen
+        // Source: https://www.tutorialspoint.com/jfreechart/jfreechart_quick_guide.htm
         this.pack();
         RefineryUtilities.centerFrameOnScreen(this);
         this.setVisible(true);
